055d19e4441928b9b39c532b54c77d6f7047fa4e
VoDangPhat <vodangphat2002@gmail.com>
2024-12-29 00:12:52 +0700

    update

--- client/src/App/View/KhachHangTiemNang/components/ImportKhachHang.jsx
+++ client/src/App/View/KhachHangTiemNang/components/ImportKhachHang.jsx
@@ -1,46 +1,68 @@
-import React, { useState } from 'react'
-import { DropzoneAreaBase } from 'mui-file-dropzone';
-import { Button } from '@mui/material';
-import { useImportKhachHangMutation } from 'src/App/Api/KhachHangTiemNangApi';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { useImportKhachHangMutation } from "src/App/Api/KhachHangTiemNangApi";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import {
+  Dropzone,
+  FileItem,
+  FullScreenPreview,
+  FileMosaic,
+} from "@files-ui/react";
 const ImportKhachHang = () => {
-    const navigate = useNavigate()
-    const [files, setFiles] = useState([]);
-    const [uploadFile, { isLoading }] = useImportKhachHangMutation();
-    const handleAdd = (newFiles) => {
-        setFiles([...files, ...newFiles]);
-      };
-    
-      const handleDelete = (deletedFile) => {
-        setFiles(files.filter((file) => file !== deletedFile));
-      };
-    const handleAutoLink = () => {
-        navigate(-1)
-    }
-    
+  const navigate = useNavigate();
 
-  const handleSubmit = async() => {
+  const [uploadFile, { isLoading }] = useImportKhachHangMutation();
+  const [files, setFiles] = React.useState([]);
+  const updateFiles = (incommingFiles) => {
+    //do something with the files
+    console.log("incomming files", incommingFiles);
+    setFiles(incommingFiles);
+    //even your own upload implementation
+  };
+  const removeFile = (id) => {
+    setFiles(files.filter((x) => x.id !== id));
+  };
+
+  const handleAutoLink = () => {
+    navigate(-1);
+  };
+
+  const handleSubmit = async () => {
     try {
-        if (files.length === 0) {
-         toast.warning("Vui lòng chọn file để import")
-          return;
-        }
-        const response = await uploadFile(files[0].file).unwrap();
-        if(response.status === 200)
-        {
-            toast.success(response.message)
-            setTimeout(() => {
-                handleAutoLink()
-            }, 2000);
-        }
-      } catch (error) {
-        toast.error("Đã có lỗi xảy ra , vui lòng liên hệ nhân viên quản trị hệ thống để nhận hỗ trợ")
+      if (files.length === 0) {
+        toast.warning("Vui lòng chọn file để import");
+        return;
       }
+      const response = await uploadFile(files[0].file).unwrap();
+      if (response.status === 200) {
+        toast.success(response.message);
+        setTimeout(() => {
+          handleAutoLink();
+        }, 2000);
+      }
+    } catch (error) {
+      toast.error(
+        "Đã có lỗi xảy ra , vui lòng liên hệ nhân viên quản trị hệ thống để nhận hỗ trợ"
+      );
+    }
   };
 
   return (
-    <div> <DropzoneAreaBase
+    <div>
+      <Dropzone
+        onChange={updateFiles}
+        label={"Upload file của bạn ..."}
+        value={files}
+      >
+        {files.map((file) => (
+          <FileMosaic key={file.id} {...file} onDelete={removeFile} info />
+        ))}
+      </Dropzone>
+      <Button style={{ margin: 10 }} variant="contained" onClick={handleSubmit}>
+        Import khách hàng
+      </Button>
+      {/* <DropzoneAreaBase
     fileObjects={files}
     onAdd={handleAdd}
     onDelete={handleDelete}
@@ -51,9 +73,9 @@ const ImportKhachHang = () => {
     showPreviewsInDropzone
     dropzoneText={"Kéo và thả file hoặc click để chọn (Kích thước tối đa 10 MB ...)"}
   />
-  <Button style={{ margin : 10 }} variant="contained" onClick={handleSubmit}>Import khách hàng</Button>
-  </div>
-  )
-}
+  <Button style={{ margin : 10 }} variant="contained" onClick={handleSubmit}>Import khách hàng</Button> */}
+    </div>
+  );
+};
 
-export default ImportKhachHang
+export default ImportKhachHang;
