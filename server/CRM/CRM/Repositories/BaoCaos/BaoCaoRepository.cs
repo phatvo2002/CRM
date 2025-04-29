1393b1fdf56d680a1f9cfa218a41cd4c31d0f356
VoDangPhat <vodangphat2002@gmail.com>
2025-04-29 07:07:00 +0700

    update

--- server/CRM/CRM/Repositories/BaoCaos/BaoCaoRepository.cs
+++ server/CRM/CRM/Repositories/BaoCaos/BaoCaoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CRM.DTO.BaoCaoDTO;
 using CRM.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRM.Repositories.BaoCaos
 {
@@ -10,40 +11,63 @@ namespace CRM.Repositories.BaoCaos
         {
             _context = context;
         }
-        public Task<BaoCaoDTO> GetBaoCaoTheoNguoiDung(DateTime tuNgay, DateTime denNgay, Guid nguoiDungId, Guid phongBanId)
+        public async Task<BaoCaoDTO> GetBaoCaoTheoNguoiDung(DateTime tuNgay, DateTime denNgay, Guid nguoiDungId, Guid phongBanId)
         {
             var thoiGianTuNgayThangTruoc = tuNgay.AddMonths(-1);
             var thoiGianDenNgayThangTruoc = tuNgay.AddMonths(-1);
 
             BaoCaoDTO baoCaoDTO = new BaoCaoDTO();
+
+            // kiểm tra nguoi dung là trưởng phòng hay nhân viên
+            var nguoiDungData = await _context.Nguoidungs.FirstOrDefaultAsync(r => r.Id == nguoiDungId);
             try
             {
-                // kiểm tra nguoi dung là trưởng phòng hay nhân viên
-                var nguoiDungData = _context.Nguoidungs.FirstOrDefault(r => r.Id == nguoiDungId);
-
-                // Nếu là nhân viên thì sử dụng hàm này 
-                if (nguoiDungData != null && nguoiDungData.CheckIsTruongPhong == false)
+                if (nguoiDungData != null)
                 {
-                    // lấy danh sách khách hàng tiềm năng 
-                    var dataKhachHangTiemNangHienTai = _context.KhachHangTiemNangs.Where(r => (tuNgay >= r.CreateAt && r.CreateAt <= denNgay) && r.NguoiDungId == nguoiDungId).ToList();
-                    var dataKhachHangTiemNangThangTruoc = _context.KhachHangTiemNangs.
-                        Where(r => (thoiGianTuNgayThangTruoc >= r.CreateAt && r.CreateAt <= thoiGianDenNgayThangTruoc) && r.NguoiDungId == nguoiDungId).ToList();
-                    if (dataKhachHangTiemNangHienTai != null)
+                    switch (nguoiDungData.CheckIsTruongPhong)
                     {
-                        baoCaoDTO.KhachHangTiemNangHienTai = dataKhachHangTiemNangHienTai.Count();
-                        baoCaoDTO.TiLeChuyenDoiKhachHangThangHienTai = dataKhachHangTiemNangHienTai.Any()
-                          ? Math.Round((decimal)dataKhachHangTiemNangHienTai.Count(r => r.IsChuyenDoi == true) / dataKhachHangTiemNangHienTai.Count() * 100, 2) : 0;
-                    }
-                    else baoCaoDTO.KhachHangTiemNangHienTai = 0; baoCaoDTO.TiLeChuyenDoiKhachHangThangHienTai = 0;
-                    if (dataKhachHangTiemNangThangTruoc != null)
-                    {
-                        baoCaoDTO.KhachHangTiemNangThangTruoc = dataKhachHangTiemNangThangTruoc.Count();
-                        baoCaoDTO.TiLeChuyenDoiKhachHangThangTruoc = (dataKhachHangTiemNangThangTruoc.Any() ? Math.Round((decimal)dataKhachHangTiemNangThangTruoc.Count(r => r.IsChuyenDoi == true) / dataKhachHangTiemNangThangTruoc.Count() * 100, 2) : 0);
-                    }
-                    else baoCaoDTO.KhachHangTiemNangThangTruoc = 0; baoCaoDTO.TiLeChuyenDoiKhachHangThangTruoc = 0;
+
+                        case false:
+                            {
+                                // lấy danh sách khách hàng tiềm năng hiện tại
+                                var dataKhachHangTiemNangHienTai = await _context.KhachHangTiemNangs.Where(r => (tuNgay >= r.CreateAt && r.CreateAt <= denNgay) && r.NguoiDungId == nguoiDungId).AsNoTracking().ToListAsync();
+                                var dataKhachHangTiemNangThangTruoc = await _context.KhachHangTiemNangs.Where(r => (thoiGianTuNgayThangTruoc >= r.CreateAt && r.CreateAt <= thoiGianDenNgayThangTruoc) && r.NguoiDungId == nguoiDungId).AsNoTracking().ToListAsync();
+                                if (dataKhachHangTiemNangHienTai != null)
+                                {
+                                    baoCaoDTO.KhachHangTiemNangHienTai = dataKhachHangTiemNangHienTai.Count();
+                                    baoCaoDTO.TiLeChuyenDoiKhachHangThangHienTai = dataKhachHangTiemNangHienTai.Any()
+                                      ? Math.Round((decimal)dataKhachHangTiemNangHienTai.Count(r => r.IsChuyenDoi == true) / dataKhachHangTiemNangHienTai.Count() * 100, 2) : 0;
+                                }
+                                else baoCaoDTO.KhachHangTiemNangHienTai = 0; baoCaoDTO.TiLeChuyenDoiKhachHangThangHienTai = 0;
+                                if (dataKhachHangTiemNangThangTruoc != null)
+                                {
+                                    baoCaoDTO.KhachHangTiemNangThangTruoc = dataKhachHangTiemNangThangTruoc.Count();
+                                    baoCaoDTO.TiLeChuyenDoiKhachHangThangTruoc = (dataKhachHangTiemNangThangTruoc.Any() ? Math.Round((decimal)dataKhachHangTiemNangThangTruoc.Count(r => r.IsChuyenDoi == true) / dataKhachHangTiemNangThangTruoc.Count() * 100, 2) : 0);
+                                }
+                                else baoCaoDTO.KhachHangTiemNangThangTruoc = 0; baoCaoDTO.TiLeChuyenDoiKhachHangThangTruoc = 0;
+
+                                // lấy danh sách cơ hội hiện tại
+                                baoCaoDTO.TongSoCoHoiHienTai = await LayTongSoTheoNguoiDungAsync(_context.CoHois, tuNgay, denNgay, nguoiDungId);
+                                // lấy danh sách cơ hội của tháng trước
+                                baoCaoDTO.TongSoCoHoiThangTruoc = await LayTongSoTheoNguoiDungAsync(_context.CoHois, thoiGianTuNgayThangTruoc, thoiGianDenNgayThangTruoc, nguoiDungId);
+                                // lấy tổng số báo giá hiện tại 
+                                baoCaoDTO.TongSoBaoGiaHienTai = await LayTongSoTheoNguoiDungAsync(_context.BaoGias, tuNgay, denNgay, nguoiDungId);
+                                // Lấy tổng số báo giá của tháng trước
+                                baoCaoDTO.TongSoBaoGiaThangTruoc = await LayTongSoTheoNguoiDungAsync(_context.BaoGias, thoiGianTuNgayThangTruoc, thoiGianTuNgayThangTruoc, nguoiDungId);
+                                // Lấy tổng số đơn hàng hiện tại 
+                                baoCaoDTO.TongSoDonHangHienTai = await LayTongSoTheoNguoiDungAsync(_context.DonHangs, tuNgay, denNgay, nguoiDungId);
+                                // Lấy tổng số đơn hàng của tháng trước
+                                baoCaoDTO.TongSoDonHangThangTruoc = await LayTongSoTheoNguoiDungAsync(_context.DonHangs, thoiGianTuNgayThangTruoc, thoiGianDenNgayThangTruoc, nguoiDungId);
 
 
+                            }
+                            break;
+                        default:
+                            baoCaoDTO = null;
+                            break;
+                    }
                 }
+
 
 
             }
@@ -53,5 +77,41 @@ namespace CRM.Repositories.BaoCaos
             }
             throw new NotImplementedException();
         }
+
+        private async Task<int> LayTongSoTheoNguoiDungAsync<T>(IQueryable<T> query, DateTime tuNgay, DateTime denNgay, Guid nguoiDungId) where T : class
+        {
+
+            var checkPermissionData = await _context.Nguoidungs.FirstOrDefaultAsync(r => r.Id == nguoiDungId);
+            if (checkPermissionData != null)
+            {
+                if (checkPermissionData.CheckIsTruongPhong == false)
+                {
+                    var result = await query.Where(r => (tuNgay >= EF.Property<DateTime>(r, "CreateAt")
+                                                         && EF.Property<DateTime>(r, "CreateAt") <= denNgay)
+                                                         && EF.Property<Guid>(r, "nguoiDungId") == nguoiDungId).AsNoTracking().ToListAsync();
+                    if (result.Count() > 0)
+                        return result.Count();
+                    else return 0;
+                }
+                else
+                {
+                    var result = await query.Where(r => (tuNgay >= EF.Property<DateTime>(r, "CreateAt")
+                                                          && EF.Property<DateTime>(r, "CreateAt") <= denNgay)
+                                                          && EF.Property<Guid>(r, "phongBanId") == checkPermissionData.MaPhongBan).AsNoTracking().ToListAsync();
+
+                    if (result.Count() > 0)
+                        return result.Count();
+                    else return 0;
+                }
+            }
+            else return 0;
+
+
+        }
+        private async Task<Double> LayTongDoanhThu<T>(IQueryable<T> query, DateTime tuNgay, DateTime denNgay, Guid nguoiDungId) where T : class
+        {
+            var checkPermissionData = await _context.Nguoidungs.FirstOrDefaultAsync(r => r.Id == nguoiDungId);
+        }
+
     }
 }
